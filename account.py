7bc59fa9557657c2f1cf75e6d1d1267814e0b6d6
allen10104 <suasallen101@gmail.com>
2026-08-10 12:23:09 -0400

    added abstract account class

---
+++ account.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class Account(ABC):
+    def __init__(self, account_id, balance):
+        self.account_id = account_id
+        self.balance = balance
+
+    def get_balance(self):
+        return self.balance
+
+    def get_id(self):
+        return self.account_id
+
+    @abstractmethod
+    def add_interest(self):
+        pass
