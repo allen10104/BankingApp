b041b16f30c43a0bbd95be28a3fb40a6aeab52ee
allen10104 <suasallen101@gmail.com>
2026-08-11 02:21:26 -0400

    Implemented POST for account creation and updated main to include all relevant routers.

---
+++ app/controllers/account_controller.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+from starlette import status
+
+from app.dependencies import account_service
+from app.models.account import Account, AccountCreate
+
+
+router = APIRouter(
+    prefix="/api/v1/accounts",
+    tags=["Accounts"]
+)
+
+
+@router.post(
+    "",
+    response_model=Account,
+    status_code=status.HTTP_201_CREATED
+)
+def create_account(account_data: AccountCreate):
+
+    account = account_service.create_account(account_data)
+
+    if account is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Customer not found"
+        )
+
+    return account
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
 from app.controllers.customer_controller import router as customer_router
-
+from app.controllers.transaction_controller import router as transaction_router
+from app.controllers.account_controller import router as account_router
 
 app = FastAPI(
     title="Rest Banking API"
@@ -9,3 +10,5 @@ app = FastAPI(
 
 
 app.include_router(customer_router)
+app.include_router(transaction_router)
+app.include_router(account_router)
