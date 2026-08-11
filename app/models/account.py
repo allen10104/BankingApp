fb7e26e5bf0d60aa3c043430e7549a175914edc8
allen10104 <suasallen101@gmail.com>
2026-08-11 13:00:37 -0400

    refactored base pydantic account models to beanie mapped database model

--- app/models/account.py
+++ app/models/account.py
@@ -1,17 +1,29 @@
-from typing import Literal
+from typing import Annotated, Literal
 
+import pymongo
+from beanie import Document, Indexed, PydanticObjectId
 from pydantic import BaseModel
 
 
 class AccountCreate(BaseModel):
-    customer_id: int
+    customer_id: PydanticObjectId
     account_type: Literal["CHECKING", "SAVINGS"]
     branch_id: int
 
 
-class Account(BaseModel):
-    id: int
-    customer_id: int
-    account_type: str
+class Account(Document):
+    customer_id: PydanticObjectId
+    account_number: Annotated[str, Indexed(unique=True)]
+    account_type: Literal["CHECKING", "SAVINGS"]
     branch_id: int
-    balance: float
+    balance: float = 0.0
+
+    class Settings:
+        name = "accounts"
+
+        indexes = [
+            [
+                ("branch_id", pymongo.ASCENDING),
+                ("balance", pymongo.ASCENDING)
+            ]
+        ]
