18144e22435d8ff0570d84e3789e2642ac355c78
allen10104 <suasallen101@gmail.com>
2026-08-11 01:40:07 -0400

    initialized and implemented account service for business logic of queries

---
+++ app/services/account_service.py
@@ -0,0 +1,16 @@
+class AccountService:
+
+    def __init__(self, account_repository, customer_repository):
+        self.account_repository = account_repository
+        self.customer_repository = customer_repository
+
+    def create_account(self, account_data):
+
+        customer = self.customer_repository.get_customer_by_id(
+            account_data.customer_id
+        )
+
+        if customer is None:
+            return None
+
+        return self.account_repository.create_account(account_data)
