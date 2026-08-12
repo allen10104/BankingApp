d617467eac86d715a42dbc6cee6b9f5b897f95fc
allen10104 <suasallen101@gmail.com>
2026-08-11 22:01:27 -0400

    refactored transaction repository queries, changing method to async

--- app/repositories/transaction_repository.py
+++ app/repositories/transaction_repository.py
@@ -1,55 +1,53 @@
-from datetime import datetime, timezone
-
+from datetime import datetime, time, timezone
 from app.models.transaction import Transaction
 
 
 class TransactionRepository:
 
-    def __init__(self):
-        self.transactions = []
-
-    def create_transfer(
+    async def create_transfer(
         self,
-        from_account_id: int,
-        to_account_id: int,
-        amount: float
+        from_account_id,
+        to_account_id,
+        amount
     ):
 
-        new_id = len(self.transactions) + 1
-
         transaction = Transaction(
-            id=new_id,
-            transaction_type="TRANSFER",
             from_account_id=from_account_id,
             to_account_id=to_account_id,
-            amount=amount,
-            created_at=datetime.now(timezone.utc)
+            amount=amount
         )
 
-        self.transactions.append(transaction)
+        await transaction.insert()
 
         return transaction
 
-    def get_transactions(
+
+    async def get_transactions(
         self,
         start_date=None,
         transaction_type=None
     ):
 
-        filtered_transactions = []
+        filters = {}
 
-        for transaction in self.transactions:
+        if start_date is not None:
 
-            if start_date is not None:
+            start_datetime = datetime.combine(
+                start_date,
+                time.min,
+                tzinfo=timezone.utc
+            )
 
-                if transaction.created_at.date() < start_date:
-                    continue
+            filters["created_at"] = {
+                "$gte": start_datetime
+            }
 
-            if transaction_type is not None:
+        if transaction_type is not None:
 
-                if transaction.transaction_type != transaction_type.upper():
-                    continue
+            filters["transaction_type"] = (
+                transaction_type.upper()
+            )
 
-            filtered_transactions.append(transaction)
-
-        return filtered_transactions
+        return await Transaction.find(
+            filters
+        ).to_list()
