64857c1c13a15a5d32099ad07fda27c3f58d6fcb
allen10104 <suasallen101@gmail.com>
2026-08-11 22:07:14 -0400

    refactored transaction service and changed methods to async

--- app/repositories/transaction_repository.py
+++ app/repositories/transaction_repository.py
@@ -3,13 +3,7 @@ from app.models.transaction import Transaction
 
 
 class TransactionRepository:
-
-    async def create_transfer(
-        self,
-        from_account_id,
-        to_account_id,
-        amount
-    ):
+    async def create_transfer(self, from_account_id, to_account_id, amount):
 
         transaction = Transaction(
             from_account_id=from_account_id,
@@ -18,36 +12,21 @@ class TransactionRepository:
         )
 
         await transaction.insert()
-
         return transaction
 
 
-    async def get_transactions(
-        self,
-        start_date=None,
-        transaction_type=None
-    ):
-
+    async def get_transactions(self, start_date=None, transaction_type=None):
         filters = {}
-
         if start_date is not None:
-
             start_datetime = datetime.combine(
                 start_date,
                 time.min,
                 tzinfo=timezone.utc
             )
 
-            filters["created_at"] = {
-                "$gte": start_datetime
-            }
+            filters["created_at"] = {"$gte": start_datetime}
 
         if transaction_type is not None:
+            filters["transaction_type"] = (transaction_type.upper())
 
-            filters["transaction_type"] = (
-                transaction_type.upper()
-            )
-
-        return await Transaction.find(
-            filters
-        ).to_list()
+        return await Transaction.find(filters).to_list()
--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -1,23 +1,31 @@
 class TransactionService:
 
-    def __init__(self, account_repository, transaction_repository):
+    def __init__(
+        self,
+        account_repository,
+        transaction_repository
+    ):
         self.account_repository = account_repository
         self.transaction_repository = transaction_repository
 
-    def transfer(self, transfer_data):
 
+    async def transfer(self, transfer_data):
         if transfer_data.amount <= 0:
             return "Transfer amount must be greater than 0"
 
         if transfer_data.from_account_id == transfer_data.to_account_id:
             return "Cannot transfer to the same account"
 
-        from_account = self.account_repository.get_account_by_id(
-            transfer_data.from_account_id
+        from_account = (
+            await self.account_repository.get_account_by_id(
+                transfer_data.from_account_id
+            )
         )
 
-        to_account = self.account_repository.get_account_by_id(
-            transfer_data.to_account_id
+        to_account = (
+            await self.account_repository.get_account_by_id(
+                transfer_data.to_account_id
+            )
         )
 
         if from_account is None or to_account is None:
@@ -29,19 +37,18 @@ class TransactionService:
         from_account.balance -= transfer_data.amount
         to_account.balance += transfer_data.amount
 
-        return self.transaction_repository.create_transfer(
+        await self.account_repository.save_account(from_account)
+        await self.account_repository.save_account(to_account)
+
+        return await self.transaction_repository.create_transfer(
             from_account.id,
             to_account.id,
             transfer_data.amount
         )
 
-    def get_transactions(
-        self,
-        start_date=None,
-        transaction_type=None
-    ):
 
-        return self.transaction_repository.get_transactions(
+    async def get_transactions(self, start_date=None, transaction_type=None):
+        return await self.transaction_repository.get_transactions(
             start_date,
             transaction_type
         )
